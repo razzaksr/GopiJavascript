02bfef42e7caf929c52c905d611dda6aa11a1dc0
razzaksr <razzaksr@gmail.com>
2022-06-18 13:55:58 +0530

    events and functions

---
+++ funs.js
@@ -0,0 +1,15 @@
+const apple=()=>{
+    alert("Normal functions")
+}
+
+const banana=(one,two)=>{
+    alert("Received "+one+" and "+two)
+}
+
+const cupCake=(alpha=2,gamma=10)=>{
+    alert("Received in cupcake "+alpha+" and "+gamma)
+}
+
+const donut=()=>{
+    return {"app":"insta","org":"omega","built":2020}
+}
